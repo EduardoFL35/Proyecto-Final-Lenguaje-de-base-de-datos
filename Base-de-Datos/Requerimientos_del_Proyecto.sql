bff577a5c09c62396cc6acad54b78e681652b49f
EduardoFL35 <>
2023-03-28 23:38:00 -0600

    Se agregaron varios requerimientos del proyecto

--- Base-de-Datos/Requerimientos_del_Proyecto.sql
+++ Base-de-Datos/Requerimientos_del_Proyecto.sql
@@ -6,6 +6,8 @@ create or replace view Mostrar_nombre_curso AS
 select nombrecurso from CURSOS;
 
 
+
+
 --- INCIDENTES
 create or replace view Mostrar_incidente AS
 select descripcion from incidentes;
